29f58c1a90ee3e69d2cf1673ba6f01d9a3b7fc14
unknown <nguyentrang0512000@gmail.com>
2021-10-31 19:53:48 +0700

    bai tap treeview hien thi file anh

---
+++ TreeViewApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace TreeViewApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnTaoCay = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.treeView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(256, 450);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // btnTaoCay
+            // 
+            this.btnTaoCay.Location = new System.Drawing.Point(284, 22);
+            this.btnTaoCay.Name = "btnTaoCay";
+            this.btnTaoCay.Size = new System.Drawing.Size(112, 41);
+            this.btnTaoCay.TabIndex = 1;
+            this.btnTaoCay.Text = "Tạo cây";
+            this.btnTaoCay.UseVisualStyleBackColor = true;
+            this.btnTaoCay.Click += new System.EventHandler(this.btnTaoCay_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TreeViewApp.Properties.Resources.loading;
+            this.pictureBox1.Location = new System.Drawing.Point(284, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(419, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(369, 416);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnTaoCay);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnTaoCay;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ TreeViewApp/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        void addNode(TreeNode parentNode, string parentDirectory, int level)
+        {
+            try
+            {
+                if (level < 5)
+                {
+                    var directoryInfo = new DirectoryInfo(parentDirectory);
+                    var directories = directoryInfo.GetDirectories();
+                    if(directories.Length > 0)
+                        foreach (var directory in directories)
+                        {
+                            TreeNode node = new TreeNode(directory.Name);
+                            parentNode.Nodes.Add(node);  
+                            addNode(node, directory.FullName, level + 1);
+                        }
+                }
+                
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnTaoCay_Click(object sender, EventArgs e)
+        {          
+            pictureBox1.Visible = true;
+
+            BackgroundWorker worker = new BackgroundWorker();
+            List<TreeNode> ls = new List<TreeNode>();
+            worker.DoWork += (t, w) =>
+            {
+                var drives = Environment.GetLogicalDrives();
+                foreach (var drive in drives)
+                {
+                    TreeNode nodeDrive = new TreeNode(drive);
+                    ls.Add(nodeDrive);
+                    addNode(nodeDrive, drive, 1);
+                }
+            };
+            worker.RunWorkerCompleted += (t, w) =>
+            {
+                treeView1.Nodes.AddRange(ls.ToArray());
+                pictureBox1.Visible = false;
+            };
+            worker.RunWorkerAsync();
+        }
+
+        List<string> fileanh = new List<string> { ".PNG", ".JPG", ".GIF" };
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            var f = Directory.GetFiles(e.Node.FullPath);
+            int i = 0;
+            foreach (var tam in f)
+            {
+                ListViewItem item = new ListViewItem();
+                if (fileanh.Contains(Path.GetExtension(tam).ToUpperInvariant()))
+                {
+                    imageList1.Images.Add(Image.FromFile(tam));
+                    item.Text = Path.GetFileName(tam);
+                    item.ImageIndex = i;
+                    i++;
+                    listView1.Items.Add(item);
+                }
+                listView1.LargeImageList = imageList1;
+            }
+        }
+    }
+}
